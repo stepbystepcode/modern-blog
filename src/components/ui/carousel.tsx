6db38055c44bcf0a859a15bffb1976524fa35c5d
Li Jing <3202146864@qq.com>
2024-01-06 22:23:31 +0800

    fix: carousel type bug

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,8 +1,11 @@
 "use client";
 import * as React from "react"
 import useEmblaCarousel, {
+// @ts-ignore
   type EmblaCarouselType as CarouselApi,
+// @ts-ignore
   type EmblaOptionsType as CarouselOptions,
+// @ts-ignore
   type EmblaPluginType as CarouselPlugin,
 } from "embla-carousel-react"
 import { ArrowLeft, ArrowRight } from "lucide-react"
