607447aaf2f3da2960aca68f78562ee104c7e06f
Li Jing <3202146864@qq.com>
2024-01-08 13:23:45 +0800

    feat: article list

--- src/app/util.ts
+++ src/app/util.ts
@@ -8,6 +8,6 @@ export const getPost = cache(async (id: string) => {
 })
 export const getPostList = cache(async () => {
     const supabase = createClient('https://vyxjlbfzsilnggewbzff.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5eGpsYmZ6c2lsbmdnZXdiemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ1MDY4MTMsImV4cCI6MjAyMDA4MjgxM30.ppfb-d4jB9ko7O1iMb0bpKNiD3lIEebN7CTD5-GWIkM')
-    const { data } = await supabase.from('posts').select('title, id, likes, views, content');
+    const { data } = await supabase.from('posts').select('title, id, likes, views, content,comments');
     return data
 })
--- src/components/article-list.tsx
+++ src/components/article-list.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import { getPostList} from "@/app/util";
 import {
@@ -13,32 +12,46 @@ import {
 
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {Eye, MessageCircleMore,ThumbsUp} from "lucide-react";
+import Link from "next/link";
+{/*{Array(8).fill(list[0])*/}
+const list = [
+    {
+        title: 'iOS 17升级密码功能，苹果给安卓手机们上了一堂课',
+        overview: '苹果iOS 15系统正式版已经发布，iOS 15系统的密码功能也将升级，苹果将会在iOS 15系统中推出iCloud密码功能，这个功能可以让用户在iPhone、iPad和Mac上使用iCloud密钥串来存储和同步密码。',
+        image: '/items/3.jpg',
+        date: '2021-09-12',
+        watch: 634,
+        comment: 23,
+        like: 12,
+    }
+]
+interface Post {
+    id: number;
+    title: string;
+    content: string;
+    views: number;
+    likes: number;
+    comments: number;
+}
+interface Props{
+    data:Post[];
 
-const ArticleList = () => {
-    const list = [
-        {
-            title: 'iOS 17升级密码功能，苹果给安卓手机们上了一堂课',
-            overview: '苹果iOS 15系统正式版已经发布，iOS 15系统的密码功能也将升级，苹果将会在iOS 15系统中推出iCloud密码功能，这个功能可以让用户在iPhone、iPad和Mac上使用iCloud密钥串来存储和同步密码。',
-            image: '/items/3.jpg',
-            date: '2021-09-12',
-            watch: 634,
-            comment: 23,
-            like: 12,
-        }
-    ]
+}
+const ArticleList = async () => {
+    const data = await getPostList();
     return (
         <>
-            {Array(8).fill(list[0])
-                .map((item, index) => {
+            {data?.map((item:Post, index:number) => {
+
                 return (
                     <div key={index} className="flex gap-3 bg-white dark:bg-[#242525] rounded-lg col-span-6 p-4">
                         <div className="aspect-[220/147] max-[576px]:h-[calc(100vw/4)] h-[9rem]  relative">
-                        <Image className="rounded-lg cursor-pointer" src={item.image} alt={item.title} layout='fill'
+                        <Image className="rounded-lg cursor-pointer" src={`/img/${item.id}`} alt={item.title} layout='fill'
                             objectFit='cover'  />
                         </div>
                         <div className="w-[75%] pt-1 flex flex-col justify-between">
-                        <div className="font-bold line-clamp-2 hover:text-blue-600 cursor-pointer">{item.title}</div>
-                        <div className="font-light line-clamp-2 max-[768px]:hidden">{item.overview}</div>
+                            <div className="font-bold line-clamp-2 hover:text-blue-600 cursor-pointer"><Link href={`/post/${item.id}`}>{item.title}</Link></div>
+                        <div className="font-light line-clamp-2 max-[768px]:hidden">{item.content}</div>
                             <div className="flex items-center justify-between">
                                 <div className="flex gap-1">
                                 <Avatar className="w-6 h-6">
@@ -48,9 +61,9 @@ const ArticleList = () => {
                                 <span className="text-sm cursor-pointer hover:text-blue-600">stepbystep</span>
                                 </div>
                                 <div className="text-gray-500 flex gap-5 scale-75" style={{fontFamily:'Rough Motion'}}>
-                                    <div className="flex max-[767px]:hidden gap-1"><Eye />{item.watch}</div>
-                                    <div className="flex max-[767px]:hidden gap-1"><MessageCircleMore />{item.comment}</div>
-                                    <div className="flex text-black gap-1 hover:text-blue-600 cursor-pointer transition-all"><ThumbsUp />{item.like}</div>
+                                    <div className="flex max-[767px]:hidden gap-1"><Eye />{item.views}</div>
+                                    <div className="flex max-[767px]:hidden gap-1"><MessageCircleMore />{item.comments}</div>
+                                    <div className="flex text-black gap-1 hover:text-blue-600 cursor-pointer transition-all"><ThumbsUp />{item.likes}</div>
                                 </div>
                             </div>
                         </div>
