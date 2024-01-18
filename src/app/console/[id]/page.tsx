8287801862cf805e9ed493353a19bdf752cbc278
Jing Li <abc1870819884@gmail.com>
2024-01-18 21:25:08 +0800

    fix: ok

--- src/app/console/[id]/page.tsx
+++ src/app/console/[id]/page.tsx
@@ -2,7 +2,7 @@ import { navList } from '@/components/admin/menu'
 export default async function AdminContent({params: { id },}: { params: { id: string } }) {
     return (
         <>
-        {navList.filter(item => item.href === id).toString()}
+        {navList.filter(item => item.href.split('/').pop() === id)[0].content}
         </>
     )
 }
